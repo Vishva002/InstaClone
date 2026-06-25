6870ab9aa3a3c60760c7449730edd34b41c8f16e
Vishva002 <nvishva117@gmail.com>
2026-06-25 14:12:10 +0530

    adding the width for separation

--- InstaClone_frontend/src/App.jsx
+++ InstaClone_frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <div className ="d-flex vh-100">
+      <div className="w-20 bg-danger" >sidebar</div>
+      <div className="w-50 bg-primary" >feed</div>
+      <div className="w-30 bg-success" >rightbar</div>
+    </div>
   )
 }
 
