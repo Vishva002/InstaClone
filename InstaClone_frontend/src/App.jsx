c12f3f96fedc15b8f9c5247951d21fb79fc47b03
Vishva002 <nvishva117@gmail.com>
2026-06-25 15:53:39 +0530

    SideBar works

--- InstaClone_frontend/src/App.jsx
+++ InstaClone_frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
+import './index.css'
+import Sidebar from './SideBar/Sidebar'
+import Feed from './feed/Feed'
+import RightBar from './rightBar/RightBar'
 
 function App() {
   return (
     <div className ="d-flex vh-100">
-      <div className="w-20 bg-danger" >sidebar</div>
-      <div className="w-50 bg-primary" >feed</div>
-      <div className="w-30 bg-success" >rightbar</div>
+      <div className="w-20 " ><Sidebar /></div>
+      <div className="w-50 " ><Feed /></div>
+      <div className="w-30 " ><RightBar /></div>
     </div>
   )
 }
